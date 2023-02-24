fda66b4cbb1fd76e341f26b136db17b6b1a7cb1b
Axizeaus <axizeaus@gmail.com>
2023-02-24 15:26:40 +0630

    cleaned up

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,23 +1,7 @@
-import myName from "./myName";
-import './style.css';
-import Icon from './icon.svg';
-import Data from './data.xml';
-import Notes from './data.csv';
-
 function component(){
   const element = document.createElement('div');
   
   element.innerHTML = myName('Ye');
-  element.classList.add('hello');
-
-  // add the image to the existing file
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element .appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -4,32 +4,7 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-
-  module:{
-    rules:[
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-    ],
   },
 };
